7ae41f0e9deaa0faa1b41492e69287752c69bab3
Abbylolo <2542111098@qq.com>
2022-01-01 17:43:42 +0800

    实现标识搜索点（指定地点写在代码中的）

--- app/src/main/java/com/example/location_baidumap/MainActivity.java
+++ app/src/main/java/com/example/location_baidumap/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.location_baidumap;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 
@@ -21,14 +26,29 @@ import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapPoi;
 import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.overlayutil.PoiOverlay;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * @author Abby
@@ -44,6 +64,9 @@ public class MainActivity extends Activity {
     private boolean isFirstLoc = true;
     private Button settingsButton;
     private SharedPreferences sharedPreferences;
+    private PoiSearch mPoiSearch;
+    private Switch markSwitch;
+    BaiduMap.OnMarkerClickListener onMarkerClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +76,8 @@ public class MainActivity extends Activity {
         settings(); //和设置页面交互
         changeMapType(); //功能一：切换地图类型
         showLocation(); //功能二：显示定位+用户设置定位模式
+        poiSearch(); //功能三：POI周边检索
+        markLoc(); //功能四：标记位置并显示经纬度
     }
 
     /**
@@ -72,6 +97,7 @@ public class MainActivity extends Activity {
         mMapView = findViewById(R.id.bMapView);
         //获取地图控制对象
         mBaiduMap = mMapView.getMap();
+        LatLng point = new LatLng(39.963175, 116.400244);
 
         /* 法二：直接在Java代码中添加MapView的方式来展示地图
         mMapView = new MapView(this);
@@ -89,12 +115,12 @@ public class MainActivity extends Activity {
         //获取SharedPreference的实例对象，指定文件的名称和操作模式
         sharedPreferences = getSharedPreferences("location_mode",MODE_PRIVATE);
         //首次运行该程序，初始化时未生成对应文件，故往文件中存储数据以生成存储文件（首次默认普通态）
-        File f = new File(
+        @SuppressLint("SdCardPath") File f = new File(
                 "/data/data/com.example.location_baidumap/shared_prefs/location_mode.xml");
         if(!f.exists()) {
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putString("locMode","普通态");
-            editor.commit();
+            editor.apply();
         }
         //获取初始/上次设置的定位模式
         locationMode = sharedPreferences.getString("locMode","普通态");
@@ -196,7 +222,7 @@ public class MainActivity extends Activity {
             int circlrColor = MainActivity.this.getColor(R.color.green_loc);
             //定位模式--NORMAL（普通态）, FOLLOWING（跟随态）, COMPASS（罗盘态）
             MyLocationConfiguration.LocationMode mCurrentMode = setLocationMode(locationMode);
-            BitmapDescriptor customMarker = BitmapDescriptorFactory.fromResource(R.drawable.loc_green);
+            BitmapDescriptor customMarker = BitmapDescriptorFactory.fromResource(R.drawable.loc_icon);
             MyLocationConfiguration myLocationConfiguration = new MyLocationConfiguration(
                     mCurrentMode, //定位模式
                     true,      //开启方向
@@ -233,6 +259,109 @@ public class MainActivity extends Activity {
             default:
                 return MyLocationConfiguration.LocationMode.NORMAL;
         }
+    }
+
+    /**
+     * 功能三：POI周边检索
+     */
+    protected void poiSearch(){
+        //1、创建POI检索实例
+        mPoiSearch = PoiSearch.newInstance();
+        //2、创建POI检索监听器
+        OnGetPoiSearchResultListener listener = new OnGetPoiSearchResultListener() {
+            //检索结果覆盖物
+            @Override
+            public void onGetPoiResult(PoiResult poiResult) {
+                //获取POI检索结果
+                if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
+                    mBaiduMap.clear();
+                    //创建PoiOverlay对象
+                    PoiOverlay poiOverlay = new PoiOverlay(mBaiduMap);
+                    //设置Poi检索数据
+                    poiOverlay.setData(poiResult);
+
+                    List<PoiInfo> allAddr = poiResult.getAllPoi();
+                    for (PoiInfo p: allAddr) {
+                        Log.d("MainActivity", "p.name--->" + p.name +"p.phoneNum" + p.phoneNum +" -->p.address:" + p.address + "p.location" + p.location);
+                    }
+
+                    //将poiOverlay添加至地图并缩放至合适级别
+                    poiOverlay.addToMap();
+                    poiOverlay.zoomToSpan();
+
+                } else {
+                    Log.d("poi","hh");
+                }
+            }
+            //获取Place详情页检索结果
+            @Override
+            public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+            }
+            @Override
+            public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
+            }
+            @Override
+            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+            }
+        };
+        //3、设置检索监听器
+        mPoiSearch.setOnGetPoiSearchResultListener(listener);
+        //4、设置PoiCitySearchOption，发起检索请求
+        /**
+         *  PoiCiySearchOption 设置检索属性
+         *  city 检索城市
+         *  keyword 检索内容关键字
+         *  pageNum 分页页码
+         */
+        mPoiSearch.searchInCity(new PoiCitySearchOption()
+                .city("杭州") //必填
+                .keyword("美食") //必填
+                .pageNum(10));
+        //5、释放检索实例
+        mPoiSearch.destroy();
+    }
+
+
+    /**
+     * 功能四：标记位置+toast显示经纬度
+     */
+    protected void markLoc() {
+        markSwitch = findViewById(R.id.mark_switch);
+        markSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    //地图事件
+                    // 调用setOnMapClickListener()方法注册监听，在监听方法中，创建一个Marker标记并添加到地图上。点击地图上的任意位置就会触发点击事件，并绘制Marker标记。
+                    mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+                        //地图单击事件回调方法
+                        @Override
+                        public void onMapClick(LatLng latLng) {
+                            BitmapDescriptor descriptor = BitmapDescriptorFactory.fromResource(R.drawable.marker_blue);
+                            OverlayOptions options = new MarkerOptions().position(latLng).icon(descriptor);
+                            mBaiduMap.addOverlay(options);
+                        }
+                        //Poi 单击事件回调方法，比如点击到地图上面的商店，公交车站，地铁站等等公共场所
+                        @Override
+                        public void onMapPoiClick(MapPoi mapPoi) {
+
+                        }
+                    });
+                    // 通过点击Marker标记可以获取该位置的一些信息
+                    onMarkerClickListener = new BaiduMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker) {
+                            Toast.makeText(getApplicationContext(), marker.getPosition().toString(), Toast.LENGTH_LONG).show();
+                            return false;
+                        }
+                    };
+                    mBaiduMap.setOnMarkerClickListener(onMarkerClickListener);
+                } else {
+                    mBaiduMap.removeMarkerClickListener(onMarkerClickListener);
+                    //mBaiduMap.clear();
+                }
+            }
+        });
     }
 
     @Override
